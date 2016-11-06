d5e8dacf3c4aa1f035f3fa3177bcb115bde1b41d
kgoralski <goralski.k@gmail.com>
2016-11-06 12:26:01 +0100

    after code review first changes

--- main/bank_dao.go
+++ main/bank_dao.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -10,53 +11,79 @@ type Bank struct {
 	Name string `json:"name" db:"name"`
 }
 
-var sqlConnection string = "admin:Admin.123@tcp(localhost:3306)/bank_db?charset=utf8"
+const DB_QUERY_FAIL = "DB_QUERY_FAIL"
+const DB_NOT_SUPPORTED = "DB_NOT_SUPPORTED"
+const ENTITY_NOT_EXIST = "ENTITY_NOT_EXIST"
 
-// This is wrong on so many levels - don't panic, ever!
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+const sqlConnection string = "admin:Admin.123@tcp(localhost:3306)/bank_db?charset=utf8"
 
-func getBanks() []Bank {
+func getBanks() ([]Bank, *HttpError) {
 	db := sqlx.MustConnect("mysql", sqlConnection)
 	var banks = []Bank{}
 	err := db.Select(&banks, "SELECT * FROM banks")
-	// handle errors, return ([]Bank, error) in this function
-	checkErr(err)
-	return banks
+	if err != nil {
+		return nil, &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	return banks, nil
 }
 
-func getBankById(id int) Bank {
+func getBankById(id int) (*Bank, *HttpError) {
 	db := sqlx.MustConnect("mysql", sqlConnection)
 	var bank = Bank{}
 	err := db.Get(&bank, "SELECT * FROM banks WHERE id=?", id)
-	checkErr(err)
-	return bank
+	if err != nil {
+		return nil, &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	return &bank, nil
 }
 
-func createBank(bank Bank) int {
+func createBank(bank Bank) (int, *HttpError) {
 	db := sqlx.MustConnect("mysql", sqlConnection)
 	result, err := db.Exec("INSERT into banks (name) VALUES (?)", bank.Name)
-	checkErr(err)
-	lastId, lastIdErr := result.LastInsertId()
-	checkErr(lastIdErr)
-	return int(lastId)
+	if err != nil {
+		return -1, &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return -1, &HttpError{err, DB_NOT_SUPPORTED, 409}
+	}
+	return int(lastId), nil
 }
 
-func deleteAllBanks() {
+func deleteAllBanks() *HttpError {
 	db := sqlx.MustConnect("mysql", sqlConnection)
-	db.Exec("TRUNCATE table banks")
+	_, err := db.Exec("TRUNCATE table banks")
+	if err != nil {
+		return &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	return nil
 }
 
-func deleteBankById(id int) {
+func deleteBankById(id int) *HttpError {
 	db := sqlx.MustConnect("mysql", sqlConnection)
-	db.Exec("DELETE from banks where id=?", id)
+	res, err := db.Exec("DELETE from banks where id=?", id)
+	if err != nil {
+		return &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	affect, err := res.RowsAffected()
+	if err != nil {
+		return &HttpError{err, DB_QUERY_FAIL, 400}
+	}
+	if affect == 0 {
+		return &HttpError{errors.New(ENTITY_NOT_EXIST), ENTITY_NOT_EXIST, 404}
+	}
+	return nil
 }
 
-func updateBank(bank Bank) Bank {
+func updateBank(bank Bank) (*Bank, *HttpError) {
 	db := sqlx.MustConnect("mysql", sqlConnection)
-	db.Exec("UPDATE banks SET name=? WHERE id=?", bank.Name, bank.Id)
-	return bank
+	res, err := db.Exec("UPDATE banks SET name=? WHERE id=?", bank.Name, bank.Id)
+	if err != nil {
+		return nil, &HttpError{err, DB_QUERY_FAIL, 409}
+	}
+	affect, err := res.RowsAffected()
+	if affect == 0 {
+		return nil, &HttpError{errors.New(ENTITY_NOT_EXIST), ENTITY_NOT_EXIST, 404}
+	}
+	return &bank, nil
 }
--- main/main_test.go
+++ main/main_test.go
@@ -4,3 +4,14 @@ func init() {
 	go startServer()
 	deleteAllBanks()
 }
+
+func panicInTest(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func checkHttpErr(err *HttpError) {
+	if err != nil {
+		panic(err)
+	}
+}
